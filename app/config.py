4c1a764afe201006320e1611ca3243b65a8f8514
Дмитрий Захаров <zds.vip1221@gmail.com>
2023-02-20 16:22:24 +0300

    Initial commit

---
+++ app/config.py
@@ -0,0 +1,6 @@
+class Config:
+    DEBUG = True
+    SECRET = 'test'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # создание конфигурации настройки приложения
